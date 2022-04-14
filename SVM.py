48488f8e6f516e49a059900e75ad4d16ae2941cf
DAVID LORENZO ALFARO <David.Lorenzo@alu.uclm.es>
2022-04-14 17:59:43 +0200

    Refactoring + saving some unnecesary computations

--- SVM.py
+++ SVM.py
@@ -6,18 +6,17 @@ class RankSVM(LinearSVC):
 
     def __init__(self, data, seed = 0):
         print('Initializing RankSVM')
-        self.__data = data
+        self.dataset = data
         super().__init__(random_state=seed, tol=1e-5) 
         self.fit()
         print('RankSVM initialized')
 
     def fit(self, sample_weight=None):
-        _data = self.__data.prepare_training_data_pairwise()
-        super(RankSVM, self).fit(_data[0], _data[1], sample_weight)
+        super(RankSVM, self).fit(*self.dataset.training_data, sample_weight)
         return self
 
     def rank(self, query: str):
-        return self.__rank(self.__data.prepare_training_data_pairwise(query)[0])
+        return np.argsort(self.dataset.get_test_data(query) @ self.coef_.ravel())
 
     def __rank(self, X):
         return np.argsort(X @ self.coef_.ravel())
@@ -25,7 +24,7 @@ class RankSVM(LinearSVC):
     def show_ranking(self, query:str, top=5):
         order = self.rank(query)
 
-        df_copy = self.__data.get_data().copy()
+        df_copy = self.dataset.data.copy()
 
         df_copy['order'] = pd.Series(order)
         df_copy = df_copy.sort_values('order',ascending=True).drop(columns=['order'])
